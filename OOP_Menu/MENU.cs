cc9aab9d8b21bc9e13ca9462480c05084eccf342
Patrik Zemánek <Zemanek.Patrik@stud.vassboskovice.cz>
2023-11-16 15:29:03 +0100

    Zobecnění 1 (třídy)

---
+++ OOP_Menu/MENU.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Menu
+{
+    public class Menu
+    {
+        public string Nadpis = "";
+        public ConsoleColor BarvaOkraje = ConsoleColor.Black;
+        public ConsoleColor BarvaPozadi = ConsoleColor.DarkYellow;
+        public ConsoleColor BarvaKurzor = ConsoleColor.White;
+        public ConsoleColor BarvaText = ConsoleColor.Yellow;
+        public int sirka = 20;
+        public string[] Polozky = new string[0];
+        
+        public void Zobrazení()
+        {
+            ZobrazOkraj();
+            ZobrazNadpis(); 
+        }
+
+        private void ZobrazNadpis()
+        {
+            Console.BackgroundColor = BarvaPozadi;
+            Console.ForegroundColor = BarvaText;
+            Console.SetCursorPosition(9, 1);
+            Console.Write("MENU");
+
+        }
+
+        private void ZobrazOkraj()
+        {
+            Console.BackgroundColor = BarvaPozadi;
+            Console.BackgroundColor = BarvaOkraje;
+            Console.Write("╔");
+            Console.Write(new String('═', sirka));
+            Console.WriteLine("╗");
+            Console.Write("║");
+            Console.Write(new String(' ', sirka));
+            Console.WriteLine("║");
+            Console.Write("╠");
+            Console.Write(new String('═', sirka));
+            Console.WriteLine("╣");
+            Console.Write("║");
+            Console.Write(new String(' ', sirka));
+            Console.WriteLine("║");
+            Console.Write("║");
+            Console.Write(new String(' ', sirka));
+            Console.WriteLine("║");
+            Console.Write("║");
+            Console.Write(new String(' ', sirka));
+            Console.WriteLine("║");
+
+            Console.Write("╚");
+            Console.Write(new String('═', sirka));
+            Console.Write("╝");
+            
+
+        }
+    }
+}
--- OOP_Menu/Program.cs
+++ OOP_Menu/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Ahoj");
+﻿//Console.ForegroundColor = ConsoleColor.Green;
+//Console.WriteLine("╔══════════════════╗");
+//Console.WriteLine("║                  ║");
+//Console.WriteLine("╠══════════════════╣");
+//Console.WriteLine("║                  ║");
+//Console.WriteLine("╠══════════════════╣");
+//Console.WriteLine("║                  ║");
+//Console.WriteLine("╠══════════════════╣");
+//Console.WriteLine("║                  ║");
+//Console.WriteLine("╚══════════════════╝");
+
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 1);
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.WriteLine("                  ");
+//Console.SetCursorPosition(8, 1);
+//Console.Write("MENU");
+
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 3);
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.WriteLine("                  ");
+//Console.SetCursorPosition(6, 3);
+//Console.Write("Položka 1");
+
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 5);
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.WriteLine("                  ");
+//Console.SetCursorPosition(6, 5);
+//Console.Write("Položka 2");
+
+//Console.ForegroundColor = ConsoleColor.Black;
+//Console.SetCursorPosition(1, 7);
+//Console.BackgroundColor = ConsoleColor.Yellow;
+//Console.WriteLine("                  ");
+//Console.SetCursorPosition(6, 7);
+//Console.Write("Položka 3");
+
+
+using OOP_Menu;
+Menu Menu = new Menu();
+Menu.Zobrazení();
+Menu.Polozky = new string[5];
+
+
+
+
+Console.CursorVisible = false;
+Console.ReadKey();
